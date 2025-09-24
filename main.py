1e5b44242df047a9114b6407300b54a315c352d9
Julienlabanane <juju@gamerjuju68.ovh>
2025-09-24 20:48:51 +0200

    Update version to 0.107 and refactor application structure

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 import sys
 import time
 import xml.etree.ElementTree as ET
+#import http
 App = Tk()
 frm = ttk.Frame(App, padding=10)
 frm.grid()
@@ -24,20 +25,19 @@ tree = ET.parse('451config.xml')
 root = tree.getroot()
 
 # Program info
-Version_release_date = "09.01.25"
+Version_release_date = "10.01.25"
 Tested_Python_version = "3.11.0, 3.12.5, 3.13.0rc2"
 Tested_Pypy_version = "3.10.14"
 isaRelease = False
 ReleaseType = "BETA"
 ReleaseName = "Maximus"
-Program_version = ReleaseType + "-0.106"
+Program_version = ReleaseType + "-0.107"
 ReleaseText = Program_version + "(" + ReleaseType + "," + Version_release_date + "," + "Tested Python Version : " + Tested_Python_version + "," + "Tested Pypy Version : " + Tested_Pypy_version + ")"
 FNSmessagetext = "Sorry, your version of 451Launcher(" + Program_version + ") unsupport this feature"
 App_website = root[0][0].text
 App_Title = root[0][1].text
 App_screensize = root[0][2].text
-App_repo_source = root[0][3].text
-websystem = root[0][4].text
+websystem = root[0][3].text
 if websystem == 'webbrowser':
     import webbrowser
 if websystem == 'pywebview':
@@ -61,7 +61,6 @@ App.resizable(width=False,height=False) #if the window is resizable
 
 # Program Code
 logging.info("<style>body {background-color: skyblue;}h3 {font-family: monospace;}</style>") #inject css
-
 def Launch_Program():
     logging.info("Launch" + " " + App_Title)
 def exit_scene():
@@ -77,12 +76,15 @@ def website_screen_content():
             webview.create()
             webview.go(App_website)
             logging.info("Webview's window launch on : " + App_website)
+def accountconnect_screen_content():
+        messagebox.showerror(title="451 Connect is comming soon",message=FNSmessagetext)
 Title = ttk.Label(frm,text="  " + "451 Launcher" + "  ",font="Arial 24",borderwidth=7).grid(column=0, row=0)
 Main_Button = ttk.Button(frm,text="Launch" + " " + App_Title,width="32",command=Launch_Program).grid(column=0, row=1)
 App_website_Button = ttk.Button(frm,text=App_Title + "'s" + " " + "Website", width="32",command=website_screen_content).grid(column=0, row=2)
 DBG_Button = ttk.Button(frm,text="About",width="32",command=dbg_screen_content).grid(column=0, row=4)
-CLOSE_Button = ttk.Button(frm,text="Close",width="32",command=exit_scene).grid(column=0, row=5)
-VER_Label = ttk.Label(frm,text=ReleaseName + ' - ' + Program_version).grid(column=0, row=6)
+ACC_Button = ttk.Button(frm,text="451Connect",width="32",command=accountconnect_screen_content).grid(column=0, row=5)
+CLOSE_Button = ttk.Button(frm,text="Close",width="32",command=exit_scene).grid(column=0, row=6)
+VER_Label = ttk.Label(frm,text=ReleaseName + ' - ' + Program_version).grid(column=0, row=7)
 
 # Program Run
 logging.info("App version: " + ReleaseText)
